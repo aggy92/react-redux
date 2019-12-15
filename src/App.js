c2926722bcd52fe6a83c0238d0c6d562aba40888
Rafał Drezner <rdrezner1@jitsolutions.pl>
2019-12-15 17:41:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          JIT React training
+        </a>
+        <p>
+          Section: <code>redux-rematch</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
