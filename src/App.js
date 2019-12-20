6ae3779bebf652c71617f83629504a79b4eabb32
Rafał Drezner <rdrezner1@jitsolutions.pl>
2019-12-21 00:29:50 +0100

    App base part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import { Dropdown, Button, Menu, Card, Col, Row } from "antd";
 
-import './App.css';
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-function App() {
+const Header = styled.header`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PostsContainer = styled.div`
+  height: 80%;
+`;
+
+const Posts = styled.div``;
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 10%;
+`;
+
+const Post = () => (
+  <Card
+    hoverable
+    cover={
+      <img
+        alt="example"
+        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+      />
+    }
+  >
+    <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Header>
+        Title
+        <Dropdown
+          trigger={["click"]}
+          placement="bottomLeft"
+          overlay={
+            <Menu>
+              <Menu.Item key="0">Profile</Menu.Item>
+              <Menu.Item key="1">My posts (number of posts)</Menu.Item>
+              <Menu.Item key="2">Friends</Menu.Item>
+              <Menu.Divider />
+              <Menu.Item key="3">Log out</Menu.Item>
+            </Menu>
+          }
         >
-          JIT React training
-        </a>
-        <p>
-          Section: <code>redux-rematch</code>
-        </p>
-      </header>
-    </div>
+          <Button type="primary" shape="circle" icon="user" size="large" />
+        </Dropdown>
+      </Header>
+      <PostsContainer>
+        Posts of User name (number of posts):
+        <Posts>
+          <Row gutter={[16, 16]}>
+            <Col span={8}>
+              <Post />
+            </Col>
+            <Col span={8}>
+              <Post />
+            </Col>
+            <Col span={8}>
+              <Post />
+            </Col>
+          </Row>
+        </Posts>
+      </PostsContainer>
+      <Footer>Footer</Footer>
+    </Container>
   );
-}
+};
 
 export default App;
